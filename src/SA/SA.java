42daa383523a64f9803098d96554790cc37d1c9e
Santiago Adán García <santiekran@hotmail.com>
2022-11-09 02:49:58 +0100

    Modificación 3

--- src/SA/SA.java
+++ src/SA/SA.java
@@ -4,7 +4,7 @@ public class SA {
 
 	public static void main(String[] args) {
 		System.out.println("Alumno:_Santiago_Adan");
-		System.out.println("Primera Modificación desde Master");
+		System.out.println("Modificación desde Master - Cambiado");
 
 	}
 
